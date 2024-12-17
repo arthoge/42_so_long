4e405131d7320008799f4ff5c151e4820b893cf9
Arthur Oger <aoger@paul-f3Ar6s9.clusters.42paris.fr>
2024-12-17 19:41:48 +0100

    done

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: aoger <aoger@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 17:13:14 by aoger             #+#    #+#             */
-/*   Updated: 2024/12/12 19:58:21 by aoger            ###   ########.fr       */
+/*   Updated: 2024/12/17 19:37:14 by aoger            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	*ft_realloc(void *ptr, size_t new_size)
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-	ft_memcpy(new_ptr, ptr, new_size);
+	ft_memcpy(new_ptr, ptr, new_size - 1);
 	free(ptr);
 	return (new_ptr);
 }
